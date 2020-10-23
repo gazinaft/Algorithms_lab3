f3606356affa1ee18152ac4fde175ab7c1068f42
gazinaft <gazinaft@gmail.com>
2020-10-23 16:00:31 +0300

    probability tests

--- lab3.cpp
+++ lab3.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <cmath>
 
 using unit_t = std::chrono::nanoseconds;
 
@@ -18,9 +19,8 @@ long filterpopulation(const std::vector<std::vector<int>>& matrix, std::vector<s
     return countDistance(matrix, population.front());
 }
 
-std::ofstream output("Res2.txt");
-std::ofstream output2("Res.txt");
-
+std::ofstream output("5percentRes.txt");
+std::ofstream output2("5percent.txt");
 
 
 
@@ -30,47 +30,8 @@ int main() {
     std::vector<std::vector<int>> population = startValue;
     long distance = 0;
     auto start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover1, lfs, rfs);
-        for (auto& item : kids) {
-            mutate1(item, 10);
-            localImprove1(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    
-    output2 << "сrossover1 mutate1 localImprove1" << '\n';
-    output << "сrossover1 mutate1 localImprove1" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover2, lfs, rfs);
-        for (auto& item : kids) {
-            mutate1(item, 10);
-            localImprove1(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover2 mutate1 localImprove1" << '\n';
-    output << "сrossover2 mutate1 localImprove1" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
 
     population = startValue;
     start = std::chrono::steady_clock::now();
@@ -78,219 +39,89 @@ int main() {
         auto [lfs, rfs] = select(MATRIX, population);
         auto kids = breedApply(crossover3, lfs, rfs);
         for (auto& item : kids) {
-            mutate1(item, 10);
-            localImprove1(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover3 mutate1 localImprove1" << '\n';
-    output << "сrossover3 mutate1 localImprove1" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover1, lfs, rfs);
-        for (auto& item : kids) {
-            mutate2(item, 10);
-            localImprove1(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover1 mutate2 localImprove1" << '\n';
-    output << "сrossover1 mutate2 localImprove1" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover2, lfs, rfs);
-        for (auto& item : kids) {
-            mutate2(item, 10);
-            localImprove1(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover2 mutate2 localImprove1" << '\n';
-    output << "сrossover2 mutate2 localImprove1" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover3, lfs, rfs);
-        for (auto& item : kids) {
-            mutate2(item, 10);
-            localImprove1(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover3 mutate2 localImprove1" << '\n';
-    output << "сrossover3 mutate2 localImprove1" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover1, lfs, rfs);
-        for (auto& item : kids) {
-            mutate1(item, 10);
+            mutate2(item, 5);
             localImprove2(item, MATRIX);
         }
         population.insert(population.end(), kids.begin(), kids.end());
         distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
+        
             output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover1 mutate1 localImprove2" << '\n';
-    output << "сrossover1 mutate1 localImprove2" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover2, lfs, rfs);
-        for (auto& item : kids) {
-            mutate1(item, 10);
-            localImprove2(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover2 mutate1 localImprove2" << '\n';
-    output << "сrossover2 mutate1 localImprove2" << '\n';
-output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover3, lfs, rfs);
-        for (auto& item : kids) {
-            mutate1(item, 10);
-            localImprove2(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover3 mutate1 localImprove2" << '\n';
-    output << "сrossover3 mutate1 localImprove2" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover1, lfs, rfs);
-        for (auto& item : kids) {
-            mutate2(item, 10);
-            localImprove2(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover1 mutate2 localImprove2" << '\n';
-    output << "сrossover1 mutate2 localImprove2" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover2, lfs, rfs);
-        for (auto& item : kids) {
-            mutate2(item, 10);
-            localImprove2(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
-    }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<unit_t>(end - start).count();
-    output2 << "сrossover2 mutate2 localImprove2" << '\n';
-    output << "сrossover2 mutate2 localImprove2" << '\n';
-    output << duration << " nanoseconds "<< '\n';
-    output << distance << '\n';
-
-    population = startValue;
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        auto [lfs, rfs] = select(MATRIX, population);
-        auto kids = breedApply(crossover3, lfs, rfs);
-        for (auto& item : kids) {
-            mutate2(item, 10);
-            localImprove2(item, MATRIX);
-        }
-        population.insert(population.end(), kids.begin(), kids.end());
-        distance = filterpopulation(MATRIX, population, populationSize);
-        if(i % 20 == 0) {
-            output2 << distance << '\n';
-        }
+        
     }
     end = std::chrono::steady_clock::now();
     duration = std::chrono::duration_cast<unit_t>(end - start).count();
     output2 << "сrossover3 mutate2 localImprove2" << '\n';
     output << "сrossover3 mutate2 localImprove2" << '\n';
-output << duration << " nanoseconds "<< '\n';
+    output << duration << " nanoseconds "<< '\n';
     output << distance << '\n';
+
+    population = startValue;
+    start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        auto [lfs, rfs] = select(MATRIX, population);
+        auto kids = breedApply(crossover3, lfs, rfs);
+        for (auto& item : kids) {
+            mutate2(item, 15);
+            localImprove2(item, MATRIX);
+        }
+        population.insert(population.end(), kids.begin(), kids.end());
+        distance = filterpopulation(MATRIX, population, populationSize);
+        
+            output2 << distance << '\n';
+        
     }
+    end = std::chrono::steady_clock::now();
+    duration = std::chrono::duration_cast<unit_t>(end - start).count();
+    output2 << "15 percent" << '\n';
+    output << "сrossover3 mutate2 localImprove2" << '\n';
+    output << duration << " nanoseconds "<< '\n';
+    output << distance << '\n';
+
+    population = startValue;
+    start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        auto [lfs, rfs] = select(MATRIX, population);
+        auto kids = breedApply(crossover3, lfs, rfs);
+        for (auto& item : kids) {
+            mutate2(item, 25);
+            localImprove2(item, MATRIX);
+        }
+        population.insert(population.end(), kids.begin(), kids.end());
+        distance = filterpopulation(MATRIX, population, populationSize);
+        
+            output2 << distance << '\n';
+        
+    }
+    end = std::chrono::steady_clock::now();
+    duration = std::chrono::duration_cast<unit_t>(end - start).count();
+    output2 << "15 percent" << '\n';
+    output << "15 percent" << '\n';
+    output << duration << " nanoseconds "<< '\n';
+    output << distance << '\n';
+
+
+    // adaptive 
+    population = startValue;
+    start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        auto [lfs, rfs] = select(MATRIX, population);
+        auto kids = breedApply(crossover3, lfs, rfs);
+        for (auto& item : kids) {
+            mutate2(item, 9 * ceil(i/100));
+            localImprove2(item, MATRIX);
+        }
+        population.insert(population.end(), kids.begin(), kids.end());
+        distance = filterpopulation(MATRIX, population, populationSize);
+        
+            output2 << distance << '\n';
+        
+    }
+    end = std::chrono::steady_clock::now();
+    duration = std::chrono::duration_cast<unit_t>(end - start).count();
+    output2 << "adaptive percent" << '\n';
+    output << "adaptive percent" << '\n';
+    output << duration << " nanoseconds "<< '\n';
+    output << distance << '\n';
+
+}
 
